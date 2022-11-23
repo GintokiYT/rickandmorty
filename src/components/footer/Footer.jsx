055c3ab255748e9eaaa7081a063472c569f25991
Renato Navarro <satoshiune21@gmail.com>
2022-11-23 11:31:16 -0500

    correction footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (  
     <ContainerFooter>
       <div className="informacion">
-        <h3>&#169; 2022 <a href="https://github.com/DevRoLoBe" target="__blank" rel="noreferrer noopener">DevRoLoBe</a> Todos los derechos reservados</h3>
+        <h3>&#169; 2022 <a href="https://github.com/GintokiYT" target="__blank" rel="noreferrer noopener">GintokiYT</a> Todos los derechos reservados</h3>
       </div>
     </ContainerFooter>
   );
